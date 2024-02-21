916961a26aed02771e52dc6e9cc4923cde3d1ba9
Afaq Ahmed <76641323+mafaqahmed@users.noreply.github.com>
2024-02-21 22:04:16 +0500

    Fixed spelling issue

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,9 +8,9 @@ import WhatsappButton from "./components/WhatsappButton";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Crystalline Water Engineers: Crystal-Clear Water Treatment Solutions",
+  title: "Crystaline Water Engineers: Crystal-Clear Water Treatment Solutions",
   description:
-    "**Get clean, safe, and reliable water with Crystalline Water Engineers.** We offer expert guidance on water treatment plant selection, complete customization, lifetime maintenance, and in-house manufacturing for high-quality, affordable plants. Ideal for businesses, residences, industries, and municipalities. Visit our website or contact us today for a free consultation.",
+    "**Get clean, safe, and reliable water with Crystaline Water Engineers.** We offer expert guidance on water treatment plant selection, complete customization, lifetime maintenance, and in-house manufacturing for high-quality, affordable plants. Ideal for businesses, residences, industries, and municipalities. Visit our website or contact us today for a free consultation.",
   keywords:
     "water treatment, water treatment plant, water purification, water filtration, custom water treatment, water treatment plant maintenance, water treatment plant manufacturer",
 };
