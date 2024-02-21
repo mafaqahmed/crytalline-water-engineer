d590479c8a8ef4dc2c3d52d834961a67600145cd
Afaq Ahmed <76641323+mafaqahmed@users.noreply.github.com>
2024-02-21 21:19:11 +0500

    Developed app branding

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,20 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      colors: {
+        primary: "#002952",
+        secondary: "#FDE428",
+        ternary: "#E2E6E9",
+        positive: "#fbbc04",
+        negative: "#21BA45",
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      screens: {
+        "mobile-sm": "380px",
       },
     },
   },
