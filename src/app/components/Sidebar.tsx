3a07fe3a644707d76c28496ff417a74de2c3733e
Afaq Ahmed <76641323+mafaqahmed@users.noreply.github.com>
2024-02-22 23:18:13 +0500

    Updated sidebar responsiveness

--- src/app/components/Sidebar.tsx
+++ src/app/components/Sidebar.tsx
@@ -1,30 +1,67 @@
-"use server";
-import constants from "../contants";
+"use client";
 import Link from "next/link";
+import { useEffect, useState } from "react";
+
+import { FaArrowCircleLeft, FaArrowCircleRight } from "react-icons/fa";
+
+import constants from "../contants";
 
 const Sidebar = () => {
+  const [showSidebar, setShowSidebar] = useState(true);
+
+  useEffect(() => {
+    if (window.innerWidth < 786) {
+      setShowSidebar(false);
+    }
+  }, []);
+
   return (
-    <div className="fixed left-0 top-1/2 transform -translate-y-1/2 flex flex-col gap-1">
-      {constants.social.map((social, ind) => (
-        <Link
-          href={social.link}
-          key={social.link + ind}
-          target="_blank"
-          className="group"
-          style={{
-            backgroundColor: social.color,
-          }}
+    <div className="fixed left-0 top-1/2 transform -translate-y-1/2 z-10">
+      <div className="relative">
+        <div
+          className={`absolute -right-7 top-1/2 transform -translate-y-1/2 transition-transform duration-300 ${
+            !showSidebar ? "-translate-x-9" : "translate-x-0"
+          } `}
         >
-          <div
-            className="p-2 md:p-3 group-hover:translate-x-3 group-hover:rounded-r-md transition-transform duration-300"
-            style={{
-              backgroundColor: social.color,
-            }}
-          >
-            <social.img className="text-xl sm:text-2xl" />
-          </div>
-        </Link>
-      ))}
+          {showSidebar ? (
+            <FaArrowCircleLeft
+              className="md:hidden text-primary text-2xl bg-white rounded-full"
+              onClick={() => setShowSidebar(false)}
+            />
+          ) : (
+            <FaArrowCircleRight
+              className="md:hidden text-primary text-2xl bg-white rounded-full"
+              onClick={() => setShowSidebar(true)}
+            />
+          )}
+        </div>
+        <div
+          className={`flex flex-col gap-1 transition-transform duration-300 ${
+            !showSidebar ? "-translate-x-10" : "translate-x-0"
+          } `}
+        >
+          {constants.social.map((social, ind) => (
+            <Link
+              href={social.link}
+              key={social.link + ind}
+              target="_blank"
+              className="group"
+              style={{
+                backgroundColor: social.color,
+              }}
+            >
+              <div
+                className="p-2 md:p-3 group-hover:translate-x-3 group-hover:rounded-r-md transition-transform duration-300"
+                style={{
+                  backgroundColor: social.color,
+                }}
+              >
+                <social.img className="text-xl sm:text-2xl" />
+              </div>
+            </Link>
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
