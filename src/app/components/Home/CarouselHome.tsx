8bd8b08def11a15991290404b131fce73529bf78
Afaq Ahmed <76641323+mafaqahmed@users.noreply.github.com>
2024-02-22 23:17:57 +0500

    Developed home carousel

---
+++ src/app/components/Home/CarouselHome.tsx
@@ -0,0 +1,57 @@
+"use client";
+import Slider from "react-slick";
+
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+const CarouselHome = () => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 300,
+    arrows: false,
+    autoplay: true,
+  };
+
+  return (
+    <div className="w-full">
+      <Slider {...settings}>
+        <div>
+          <div
+            className="w-full h-[30rem]"
+            style={{
+              backgroundImage: 'url("/images/image1.jpg")',
+              backgroundRepeat: "no-repeat",
+              backgroundSize: "cover",
+              backgroundPosition: "center",
+            }}
+          ></div>
+        </div>
+        <div>
+          <div
+            className="w-full h-[30rem]"
+            style={{
+              backgroundImage: 'url("/images/image2.jpg")',
+              backgroundRepeat: "no-repeat",
+              backgroundSize: "cover",
+              backgroundPosition: "center",
+            }}
+          ></div>
+        </div>
+        <div>
+          <div
+            className="w-full h-[30rem]"
+            style={{
+              backgroundImage: 'url("/images/image3.jpg")',
+              backgroundRepeat: "no-repeat",
+              backgroundSize: "cover",
+              backgroundPosition: "center",
+            }}
+          ></div>
+        </div>
+      </Slider>
+    </div>
+  );
+};
+
+export default CarouselHome;
