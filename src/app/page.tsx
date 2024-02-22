d6e13ef4b50392bcce829486d632755911d5b98f
Afaq Ahmed <76641323+mafaqahmed@users.noreply.github.com>
2024-02-22 23:18:55 +0500

    Added carousel and about section in home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
-import Image from "next/image";
+import CarouselHome from "./components/Home/CarouselHome";
+import HomeAbout from "./components/Home/HomeAbout";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Hello World
+    <main className="w-full min-h-[78vh] bg-white text-black">
+      <CarouselHome />
+      <HomeAbout />
     </main>
   );
 }
