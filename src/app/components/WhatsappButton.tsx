6321751cdf83144bc2c26a7cceecaa7e144adc29
Afaq Ahmed <76641323+mafaqahmed@users.noreply.github.com>
2024-02-22 23:19:16 +0500

    Updated whatsapp button design

--- src/app/components/WhatsappButton.tsx
+++ src/app/components/WhatsappButton.tsx
@@ -1,17 +1,12 @@
-import Image from "next/image";
 import Link from "next/link";
+
+import { SiWhatsapp } from "react-icons/si";
 
 const WhatsappButton = () => {
   return (
-    <div className="fixed bottom-5 right-5 md:bottom-10 md:right-10">
+    <div className="fixed bottom-5 right-5 md:bottom-10 md:right-10 md:hidden z-10">
       <Link href="https://wa.link/dmr42e" target="_blank">
-        <Image
-          src="/images/whatsapp.png"
-          alt="whatsapp"
-          width={55}
-          height={55}
-          className="rounded-full"
-        />
+        <SiWhatsapp className="rounded-lg text-[2.7rem] bg-[#8ABC3A] p-[6px]" />
       </Link>
     </div>
   );
