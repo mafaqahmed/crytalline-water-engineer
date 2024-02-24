5783649598c1bdc8e019d4bb38c493f3d92d834f
Afaq Ahmed <76641323+mafaqahmed@users.noreply.github.com>
2024-02-24 08:52:14 +0500

    Updated progress bar styling

--- src/app/components/ProgressBar.tsx
+++ src/app/components/ProgressBar.tsx
@@ -2,13 +2,13 @@ import React from "react";
 
 const ProgressBar = ({ progress }: { progress: number }) => {
   return (
-    <div className="w-full rounded-full overflow-hidden h-2 border">
+    <div className="w-full bg-gray-100 rounded-full overflow-hidden h-2 border border-gray-300">
       <div
         className="h-full"
         style={{
           width: progress + "%",
           backgroundImage:
-            "repeating-linear-gradient(-45deg, blue 0%, blue 1%, #3588EE 1%, #3588EE 2%)",
+            "repeating-linear-gradient(-45deg, blue 0px, blue 4px, #3588EE 4px, #3588EE 8px)",
         }}
       ></div>
     </div>
