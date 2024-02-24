d74bcaecce1ee678f97f21d26469698799ce3e75
Afaq Ahmed <76641323+mafaqahmed@users.noreply.github.com>
2024-02-24 08:08:59 +0500

    Added testimonials data

---
+++ src/app/constants/home.ts
@@ -0,0 +1,38 @@
+const constants = {
+  testimonials: [
+    {
+      label: "Combination of latest and best technologies",
+      progress: 100,
+    },
+    {
+      label: "Very competetive price",
+      progress: 100,
+    },
+    {
+      label: "Exceptional after sale",
+      progress: 100,
+    },
+    {
+      label: "Latest products with best quality",
+      progress: 100,
+    },
+    {
+      label: "Service and Support",
+      progress: 100,
+    },
+    {
+      label: "Flawless execution and start up",
+      progress: 100,
+    },
+    {
+      label: "Experienced and Expert Team",
+      progress: 100,
+    },
+    {
+      label: "And last but not least, satisfied customers",
+      progress: 100,
+    },
+  ],
+};
+
+export default constants;
