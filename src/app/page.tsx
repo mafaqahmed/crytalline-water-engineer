91a4f64d44b9864c61e119aa9231aa82fb496c7c
Afaq Ahmed <76641323+mafaqahmed@users.noreply.github.com>
2024-02-24 08:10:16 +0500

    Added taxation, feature and testimonial sections in home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,17 @@
 import CarouselHome from "./components/Home/CarouselHome";
+import FeaturesHome from "./components/Home/FeaturesHome";
 import HomeAbout from "./components/Home/HomeAbout";
+import TaxHome from "./components/Home/TaxHome";
+import TestimonialHome from "./components/Home/TestimonialHome";
 
 export default function Home() {
   return (
     <main className="w-full min-h-[78vh] bg-white text-black">
       <CarouselHome />
       <HomeAbout />
+      <TaxHome />
+      <FeaturesHome />
+      <TestimonialHome />
     </main>
   );
 }
