2169ce3ef12353351a4dc6fe022cc47e7f3cd66d
Afaq Ahmed <76641323+mafaqahmed@users.noreply.github.com>
2024-02-25 15:59:20 +0500

    updated feature carousel cards icons color

--- src/app/components/Home/CarouselHomeTwo.tsx
+++ src/app/components/Home/CarouselHomeTwo.tsx
@@ -17,36 +17,42 @@ const CarouselHomeTwo = () => {
       description:
         "Pure water, Pakistan-wide. Crystaline Water Engineer cover every corner. Clean water, guaranteed.",
       icon: AiOutlineGlobal,
+      color: "#194DA8",
     },
     {
       title: "Industry's Proven Technologies",
       description:
         "Industry-leading, proven technologies for unmatched performance, reliability, and innovation.",
       icon: VscWorkspaceTrusted,
+      color: "#E878E8",
     },
     {
       title: "Renowned Water Solutions",
       description:
         "Renowned for manufacturing top-tier water treatment plants, setting industry standards worldwide.",
       icon: SiTrustpilot,
+      color: "#2F82D6",
     },
     {
       title: "Advanced Filtration methods",
       description:
         "Innovative filtration methods ensuring unparalleled water quality, purity, and sustainability.",
       icon: ImFilter,
+      color: "#8ABC3A",
     },
     {
       title: "Advanced Multiple Serving Areas",
       description:
         "Sophisticated multi-service areas with advanced capabilities for enhanced performance and efficiency.",
       icon: GrTechnology,
+      color: "#E878E8",
     },
     {
       title: "Lifetime Maintenance Service",
       description:
         "Lifetime maintenance ensures enduring performance, peace of mind, keeping systems running smoothly.",
       icon: GiAutoRepair,
+      color: "#194DA8",
     },
   ];
   const settings = {
@@ -86,7 +92,10 @@ const CarouselHomeTwo = () => {
               key={el.title + ind}
               className="space-y-4 px-5 border-r-2 border-l-2 border-gray-400 h-52 md:h-48 overflow-hidden"
             >
-              <el.icon className="mx-auto text-5xl text-[#194DA8]" />
+              <el.icon
+                className="mx-auto text-5xl"
+                style={{ color: el.color }}
+              />
               <div className="text-center">
                 <p className="text-primary font-bold text-xl mb-1">
                   {el.title}
