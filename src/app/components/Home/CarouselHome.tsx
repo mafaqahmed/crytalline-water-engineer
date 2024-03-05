5340cfed0f5af0b891097c11a155e3194ab182b1
Afaq Ahmed <76641323+mafaqahmed@users.noreply.github.com>
2024-03-06 04:10:54 +0500

    Updated carousel home styling

--- src/app/components/Home/CarouselHome.tsx
+++ src/app/components/Home/CarouselHome.tsx
@@ -1,6 +1,8 @@
 "use client";
 import Slider from "react-slick";
 
+import { FaArrowRight } from "react-icons/fa";
+
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
@@ -17,9 +19,30 @@ const CarouselHome = () => {
   return (
     <div className="w-full">
       <Slider {...settings}>
-        <div>
+        <div className="relative w-full h-[91vh]">
+          <div className="absolute left-10 bottom-10 md:left-28 md:bottom-28 z-50">
+            <div className="mb-10 md:mb-16">
+              <h2 className="text-white font-bold text-3xl md:text-5xl">
+                100% Trusted Company
+              </h2>
+              <p className="text-white ml-2">#01 Choice all over Pakistan</p>
+            </div>
+            <button className="flex items-center gap-3 bg-primary text-white rounded px-5 py-3">
+              Get Started
+              <span>
+                <FaArrowRight />
+              </span>
+            </button>
+          </div>
           <div
-            className="w-full h-[91vh]"
+            className="w-full h-full absolute top-0 left-0 z-10"
+            style={{
+              background:
+                "linear-gradient(rgba(255,255,255,0), rgba(255,165,0,0.5))",
+            }}
+          />
+          <div
+            className="w-full h-full absolute top-0 left-0"
             style={{
               backgroundImage: 'url("/images/image1.jpg")',
               backgroundRepeat: "no-repeat",
@@ -28,9 +51,33 @@ const CarouselHome = () => {
             }}
           ></div>
         </div>
-        <div>
+        <div className="relative w-full h-[91vh]">
+          <div className="absolute left-10 bottom-10 md:left-28 md:bottom-28 z-50">
+            <div className="mb-10 md:mb-16">
+              <h2 className="text-white font-bold text-3xl md:text-5xl">
+                Vast Variety of Water Treatment Plants
+              </h2>
+              <p className="text-white ml-2">
+                Deal in all kind of water treatment plants with full
+                customization
+              </p>
+            </div>
+            <button className="flex items-center gap-3 bg-primary text-white rounded px-5 py-3">
+              Contact Us
+              <span>
+                <FaArrowRight />
+              </span>
+            </button>
+          </div>
           <div
-            className="w-full h-[91vh]"
+            className="w-full h-full absolute top-0 left-0 z-10"
+            style={{
+              background:
+                "linear-gradient(rgba(255,255,255,0), rgba(255,165,0,0.5))",
+            }}
+          />
+          <div
+            className="w-full h-full absolute top-0 left-0"
             style={{
               backgroundImage: 'url("/images/image2.jpg")',
               backgroundRepeat: "no-repeat",
@@ -39,9 +86,32 @@ const CarouselHome = () => {
             }}
           ></div>
         </div>
-        <div>
+        <div className="relative w-full h-[91vh]">
+          <div className="absolute left-10 bottom-10 md:left-28 md:bottom-28 z-50">
+            <div className="mb-10 md:mb-16">
+              <h2 className="text-white font-bold text-3xl md:text-5xl">
+                Get a Free Quote
+              </h2>
+              <p className="text-white ml-2">
+                Our team is 24/7 available for assistance
+              </p>
+            </div>
+            <button className="flex items-center gap-3 bg-primary text-white rounded px-5 py-3">
+              Meet Us
+              <span>
+                <FaArrowRight />
+              </span>
+            </button>
+          </div>
           <div
-            className="w-full h-[91vh]"
+            className="w-full h-full absolute top-0 left-0 z-10"
+            style={{
+              background:
+                "linear-gradient(rgba(255,255,255,0), rgba(255,165,0,0.5))",
+            }}
+          />
+          <div
+            className="w-full h-full absolute top-0 left-0"
             style={{
               backgroundImage: 'url("/images/image3.jpg")',
               backgroundRepeat: "no-repeat",
